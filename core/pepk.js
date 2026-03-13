14b68d8b78c467e0b7b4d1a3281f50f4ce7048ad
zhouzhenliang <zhouzhenliang@163.com>
2026-03-13 17:29:00 +0800

    支持内部pepk.js

--- core/pepk.js
+++ core/pepk.js
@@ -1,10 +1,19 @@
 const { exec } = require("child_process")
+const { app } = require("electron")
 
 exports.run = (event, args) => {
     console.log("args = ", args)
+
     return new Promise((resolve, reject) => {
+        let jarPath;
+        if (app.isPackaged) {
+            jarPath = path.join(process.resourcesPath, "pepk/pepk.jar");
+        } else {
+            jarPath = path.join(__dirname, "pepk/pepk.jar");
+        }
+
         const cmd = `
-java -jar pepk/pepk.jar \
+java -jar ${jarPath} \
 --keystore="${args.keystore}" \
 --alias="${args.alias}" \
 --output="${args.outputDir}/output.zip" \
