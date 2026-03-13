e898becc98f8fff6b876c0c17c1a3875813d0713
zhouzhenliang <zhouzhenliang@163.com>
2026-03-13 17:51:38 +0800

    整理目录

--- app/main.js
+++ app/main.js
@@ -14,7 +14,7 @@ function createWindow() {
         }
     })
 
-    win.loadFile("src/renderer/index.html")
+    win.loadFile("app/renderer/index.html")
 
     // 调试用
     // win.webContents.openDevTools()
